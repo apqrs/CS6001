800644f56abd776292dcad4e990205a43c68851d
therealpj24 <theshishir01@gmail.com>
2021-08-28 10:11:30 +0545

    testing vs

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ import selenium
 import cv2
 
 print("Hello world")
-#added comment
+# added comment
+cap = cv2.imread("img.jpg")
+
+cap = cv2.resize(cap, (1200, 720))
+cv2.circle(cap, (20, 20), 20, (255, 0, 255), 3)
+
+cv2.imshow("Image", cap)
+cv2.waitKey()
+
+print("Hello WOrld")
